d74226205478f5329057ee7f6ce9c86e548ba649
Roni Biswas <ronibiswas068@gmail.com>
2025-07-19 00:21:52 +0600

    added 404 not found page and loading spinner

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center h-[calc(100vh-80px)]">
+      <span className="loading loading-bars loading-lg"></span>
+    </div>
+  );
+};
+
+export default Loading;
